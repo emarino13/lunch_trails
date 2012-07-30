0e816da344a96c9025a22a7eefa6feb851175ceb
Elaine Marino <elaine.c.marino@gmail.com>
2012-07-30 15:43:26 -0600

    Started rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchTrails::Application.config.secret_token = 'e8245e60fd70786a70bb141994fffa3ef27c79a9a1785eeed6ebd90fb1b4635dbf737c71d3cb7e367152f0228e3c966f0d8679bada80792e116bcfeb6beac4bd'
