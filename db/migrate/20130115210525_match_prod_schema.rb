a04946ace9cdbb055422bf1a21a23798597693e4
Kevin Weller <kweller@asapwebsoft.com>
2013-01-24 10:48:49 -0700

    Create match_prod_schema migration skeleton.

--- db/migrate/20130115210525_match_prod_schema.rb
+++ db/migrate/20130115210525_match_prod_schema.rb
@@ -1,5 +1,11 @@
 class MatchProdSchema < ActiveRecord::Migration
   def up
+    drop_table :lunchtrucks
+    drop_table :users
+    
+    create_table :trucks, :force => true do |t|
+      t.
+    end
   end
 
   def down
