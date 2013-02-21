3a71b11b7a50be57268787aa0657924149f45812
Elaine Marino <elaine.c.marino@gmail.com>
2013-02-21 12:19:59 -0700

    revised seed file and models to get seed data in database

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,4 +1,4 @@
-class Lunchtruck < ActiveRecord::Base
+class Truck < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible(:truck_name,
                   :password,
@@ -7,9 +7,9 @@ class Lunchtruck < ActiveRecord::Base
                   :email_address,
                   :web_address,
                   :facebook_name,
-                  :post_to_facebook?,
+                  :post_to_facebook,
                   :twitter_handle,
-                  :post_to_twitter?)
+                  :post_to_twitter)
 
   validates(:truck_name, :presence => true)
   validates(:password, :presence => true)
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Lunchtruck.create!(:truck_name => 'Comida',
+Truck.create!(:truck_name => 'Comida',
                    :password => 'user1',
-                   :contact_name => 'Rayme Rossello'
+                   :contact_name => 'Rayme Rossello',
                    :phone => '720-204-6455',
                    :email_address => 'info@eatcomida.com',
                    :web_address => 'www.eatcomida.com',
                    :twitter_handle => '@eatcomida',
                    :facebook_name => 'http://www.facebook.com/pages/Boulder-CO/Comida/288893096153',
-                   :post_to_facebook? => 'yes',
-                   :post_to_twitter? => 'yes')
+                   :post_to_facebook => true,
+                   :post_to_twitter => true)
 
-Lunchtruck.create!(:truck_name => 'Cheese Louise',
+Truck.create!(:truck_name => 'Cheese Louise',
                    :password => 'user2',
                    :contact_name => 'kc',
+                   :phone => '480-229-8946',
                    :email_address => 'kc@cheeselouisefoodtruck.com',
                    :web_address => 'http://cheeselouisefoodtruck.com',
                    :facebook_name => 'https://www.facebook.com/CheeseLouiseFoodTruck',
-                   :post_to_facebook => 'yes',
+                   :post_to_facebook => true,
                    :twitter_handle => '@cheesefoodtruck',
-                   :post_to_twitter => 'yes')
+                   :post_to_twitter => true)
 
-Lunchtruck.create!(:truck_name => 'The Tasterie Truck',
+Truck.create!(:truck_name => 'The Tasterie Truck',
                    :password => 'user3',
                    :contact_name => 'Nathan',
                    :phone => '222-222-2222',
@@ -35,6 +36,6 @@ Lunchtruck.create!(:truck_name => 'The Tasterie Truck',
                    :web_address => 'www.thetasterietruck.com',
                    :twitter_handle => '@TasterieTruck',
                    :facebook_name => 'http://facebook.com/TheTasterieTruck',
-                   :post_to_twitter => 'yes',
-                   :post_to_facebook => 'yes')
+                   :post_to_twitter => true,
+                   :post_to_facebook => true)
 
